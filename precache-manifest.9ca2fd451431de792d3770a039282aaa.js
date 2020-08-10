223314af28d8a2c1b8f909e6cfb33779d87f2ad0
abhi <abhideepak22@gmail.com>
2020-08-10 19:17:43 +0530

    Updates

---
+++ precache-manifest.9ca2fd451431de792d3770a039282aaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eda7efff74b64035a888e6d7150fd37",
+    "url": "/newwelcome/index.html"
+  },
+  {
+    "revision": "cf88b3e0ba6deb3995ec",
+    "url": "/newwelcome/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "80d94870d467081edead",
+    "url": "/newwelcome/static/css/main.8b076cef.chunk.css"
+  },
+  {
+    "revision": "cf88b3e0ba6deb3995ec",
+    "url": "/newwelcome/static/js/2.6e822bc9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/newwelcome/static/js/2.6e822bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80d94870d467081edead",
+    "url": "/newwelcome/static/js/main.4d05ebd5.chunk.js"
+  },
+  {
+    "revision": "78d682e8331d07dbd6c0",
+    "url": "/newwelcome/static/js/runtime-main.3cf5e0ea.js"
+  }
+]);
